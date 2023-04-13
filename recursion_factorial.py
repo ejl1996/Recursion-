c6d4c0a151eb7ac22b597ffa7a26d8754e44f05c
Emma Lee <62850487+ejl1996@users.noreply.github.com>
2023-04-13 02:14:52 -0700

    Implement recursion functions

--- recursion_factorial.py
+++ recursion_factorial.py
@@ -1,11 +1,11 @@
-def factorial(n):
+def factorial(value):
     # Base case: if n is 0 or 1, return 1
-    if n == 0 or n == 1:
+    if value == 0 or value == 1:
         return 1
 
     # Recursive case: if n is greater than 1, call the function with n-1 and multiply by n
     else:
-        return n * factorial(n - 1)
+        return value * factorial(value - 1)
 
 def main():
     result = factorial(5)
